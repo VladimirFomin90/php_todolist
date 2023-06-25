7e7651739e152e4b858567756294f7c1a831f46e
VladimirFomin90 <vladimirfomin90@yandex.ru>
2023-06-25 13:12:07 +0600

    init PHP Todo List

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once("addTodo.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Todo List</title>
+</head>
+<body>
+    <h3>PHP Todo List</h3>
+</body>
+</html>
